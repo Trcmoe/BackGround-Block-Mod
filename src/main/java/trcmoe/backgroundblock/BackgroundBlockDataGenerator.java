6ede6c430191292fc935f0bd285c6a4f65d50504
Trcmoe <88249678+Trcmoe@users.noreply.github.com>
2024-04-14 20:17:23 +0800

    Initial commit

---
+++ src/main/java/trcmoe/backgroundblock/BackgroundBlockDataGenerator.java
@@ -0,0 +1,11 @@
+package trcmoe.backgroundblock;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BackgroundBlockDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/trcmoe/backgroundblock/ModClient.java
@@ -0,0 +1,10 @@
+package trcmoe.backgroundblock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
